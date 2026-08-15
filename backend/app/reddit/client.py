97aa8819d9981a2cccb1d30d83656e57e4f629c3
souparno-nag <souparnonag2004@gmail.com>
2026-08-15 11:29:35 +0530

    onfigure ruff lint and format rules in backend/pyproject.toml

--- backend/app/reddit/client.py
+++ backend/app/reddit/client.py
@@ -1,22 +1,23 @@
-import praw
 import os
+
+import praw
 from dotenv import load_dotenv
 from praw.reddit import Reddit, Subreddit
 
 # Setting up environment variables
-load_dotenv() # Load all environment variables
+load_dotenv()  # Load all environment variables
 REDDIT_CLIENT_ID = os.getenv("REDDIT_CLIENT_ID")
 REDDIT_CLIENT_SECRET = os.getenv("REDDIT_CLIENT_SECRET")
 REDDIT_USER_AGENT = os.getenv("REDDIT_USER_AGENT")
 
+
 # Set up Reddit client
 def setup_reddit_client() -> Reddit:
     reddit = praw.Reddit(
-        client_id=REDDIT_CLIENT_ID,
-        client_secret=REDDIT_CLIENT_SECRET,
-        user_agent=REDDIT_USER_AGENT
+        client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET, user_agent=REDDIT_USER_AGENT
     )
     return reddit
+
 
 # Obtain a subreddit
 def obtain_subreddit(subreddit_name: str) -> Subreddit:
